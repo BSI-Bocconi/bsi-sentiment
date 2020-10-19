51a123fd9f347c9ee15513cd1ed0df3424ce11ca
scortino <stefanocortinovis17@gmail.com>
2020-10-19 23:39:23 +0200

    Removed config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import argparse
-import configparser
-
-# TODO: config.py script should create config.ini file according to the arguments passed by the user from the CLI
-# e.g. python config_ini -d ./config/my_config.ini -q "tweet query" --until="2020-10-16"
-# The config.ini file will contain all the arguments necessary to run script bsi_sentiment.py with something like
-# e.g. python bsi_sentiment.py -f ./config/my_config.ini
